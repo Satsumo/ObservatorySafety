e7320ec414fa771a09b92bf244c793841d7536a7
Satwant Singh Sandhu <sat.sandhu@yahoo.co.uk>
2026-07-30 11:17:30 +0100

    Removed string representations for Monitor and Status types as they were just causing confusion and complexity

--- src/ObservatorySafety.Core/Status/MonitorType.cs
+++ src/ObservatorySafety.Core/Status/MonitorType.cs
@@ -1,20 +1,11 @@
 ﻿
-using System.Runtime.Serialization;
-
 namespace ObservatorySafety.Core.Status
 {
   public enum MonitorType
   {
-    [EnumMember(Value = "Power.Status")]
-    PowerStatus,
-
-    [EnumMember(Value = "DarkDragon.Mount.Sensor")]
     DarkDragonMountSensor,
-
-    [EnumMember(Value = "Dragonfly")]
     Dragonfly,
-
-    [EnumMember(Value = "NINA")]
-    NINA
+    NINA,
+    PowerStatus
   }
 }
